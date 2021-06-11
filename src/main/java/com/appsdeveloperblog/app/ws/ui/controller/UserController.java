56dc5ce1a5414e014fee27bffe9c65b3e6290de3
rameshjayaraj <rameshjayaraj@gmail.com>
2021-06-11 08:06:11 +0530

    Adding Req Parameters and return response object

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
 
     @GetMapping
-    public String getUser(){
-        return "Get User method was called...";
+    public String getUser(@RequestParam(value = "page", defaultValue = "1") int page,
+                          @RequestParam(value = "limit", defaultValue = "50") int limit,
+                          @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort){
+        return "Get User method was called with page = "+ page + " with limit = " +limit +" and sort = "+sort;
+    }
+
+    @GetMapping(path="/{userId}")
+    public UserRest getUser(@PathVariable String userId){
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail("ramesh@gmail.com");
+        returnValue.setFirstName("ramesh");
+        returnValue.setLastName("jayaraj");
+        returnValue.setUserId("rameshjayaraj");
+        return returnValue;
     }
 
     @PostMapping
